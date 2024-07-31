f336b641a849fe0d47d4f57cd88d7bbfff3eb77a
Yuliia Ivaniuk <ivanykjula@gmail.com>
2024-07-31 16:47:02 +0300

    Add navbar and contact page

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col1,  col2 = st.columns([1, 0.2, 2])
 
 with col1:
     st.image("images/photo1.jpg")
@@ -13,10 +13,10 @@ with col2:
     content = """Hi I am Yuliia! I am Pythin programmer. I study Computer Sience and try to do my best to find well-paid job"""
     st.info(content)
 
-content2 = """"Below you can find some of the apps I have build in Python. Feel free to contact me!"""
+content2 = """Below you can find some of the apps I have build in Python. Feel free to contact me!"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col2, col4 = st.columns([1.5, 0.5, 1.5])
 
 data_frame = pandas.read_csv("data.csv", sep=";")
 
