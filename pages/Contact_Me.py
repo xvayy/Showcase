e8b20c81f531408b9c338ab45a9687ab76dece6a
Yuliia Ivaniuk <ivanykjula@gmail.com>
2024-07-31 22:47:31 +0300

    Add automatic email title

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,13 +1,19 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email adress")
-    message = st.text_area("Your message")
+    my_message = st.text_area("Your message")
+    letter = f"""\
+Subject: Meeting
+{my_message}
+From {user_email}
+"""
     button = st.form_submit_button()
     if button:
-        print("I was pressed")
-
+        send_email(letter)
+        st.info("Your email was send succesfully")
 
 #oxno eybz nwdc bezz
--- send_email.py
+++ send_email.py
@@ -1,21 +1,15 @@
 import smtplib, ssl
-import ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    user_name = "ivanykjula@gmail.com"
+    password = "oxnoeybznwdcbezz"
 
-user_name = "ivanykjula@gmail.com"
-password = "oxnoeybznwdcbezz"
-receiver = "yuliiaivaniuk8@gmail.com"
-context = ssl.create_default_context()
+    receiver = "yuliiaivaniuk8@gmail.com"
+    context = ssl.create_default_context()
 
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver, message)
 
-message = """
-Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(user_name, password)
-    server.sendmail(user_name, receiver, message)
