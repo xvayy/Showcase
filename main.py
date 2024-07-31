aaed718f694f0bdae25ff77bad06100e116f82b8
Yuliia Ivaniuk <ivanykjula@gmail.com>
2024-07-31 14:02:26 +0300

    Render 2 colums that contains title, description, url links and images my projects

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -11,3 +12,26 @@ with col2:
     st.title("Yuliia Ivaniuk")
     content = """Hi I am Yuliia! I am Pythin programmer. I study Computer Sience and try to do my best to find well-paid job"""
     st.info(content)
+
+content2 = """"Below you can find some of the apps I have build in Python. Feel free to contact me!"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+data_frame = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in data_frame[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row["image"]}")
+        st.link_button(label="Link to the project", url=row["url"])
+
+
+
+with col4:
+    for index, row in data_frame[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row["image"]}")
+        st.link_button(label="Link to the project", url=row["url"])
